8661c05046d47a4f3823e16315f548e4b0dbe283
FireLifeDanik28 <lifedanik2006@gmail.com>
2026-02-17 12:04:03 +0100

    Bubble

--- Sortowanie/BubbleSort.cs
+++ Sortowanie/BubbleSort.cs
@@ -8,5 +8,27 @@ namespace Sortowanie
 {
     internal class BubbleSort
     {
+        static void Sort(int[] tablica)
+        {
+            int temporary;
+
+            for (int j = 0; j < tablica.Length - 1; j++)
+            {
+                for (int i = 0; i < tablica.Length - j - 1; i++)
+                {
+                    if (tablica[i] > tablica[i + 1])
+                    {
+                        temporary = tablica[i + 1];
+                        tablica[i + 1] = tablica[i];
+                        tablica[i] = temporary;
+                    }
+                }
+            }
+
+            foreach (int n in tablica)
+            {
+                Console.Write($"{n} ");
+            }
+        }
     }
 }
