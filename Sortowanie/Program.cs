83feb6de44477b8396ef56b002e967c620e6573f
FireLifeDanik28 <lifedanik2006@gmail.com>
2026-02-17 12:02:08 +0100

    Add project files.

---
+++ Sortowanie/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sortowanie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
