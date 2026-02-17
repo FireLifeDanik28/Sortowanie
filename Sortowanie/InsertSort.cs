a24bcbc534bd5658c2fcb234289893a11cbb4448
FireLifeDanik28 <lifedanik2006@gmail.com>
2026-02-17 12:05:13 +0100

    Insert

--- Sortowanie/InsertSort.cs
+++ Sortowanie/InsertSort.cs
@@ -8,5 +8,41 @@ namespace Sortowanie
 {
     internal class InsertSort
     {
+        static void Sort(int[] tablica)
+        {
+            int[] tablica2 = new int[tablica.Length];
+
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                if (i != 0)
+                {
+                    tablica2[i] = tablica[i];
+                    for (int j = i; j > 0; j--)
+                    {
+                        if (tablica2[j - 1] > tablica2[j])
+                        {
+                            int temp = tablica2[j];
+                            tablica2[j] = tablica2[j - 1];
+                            tablica2[j - 1] = temp;
+                        }
+
+                    }
+                }
+                else
+                {
+                    //pierwszy element
+                    tablica2[i] = tablica[i];
+                }
+            }
+            foreach (int n in tablica)
+            {
+                Console.Write($"{n} ");
+            }
+            Console.WriteLine("\n");
+            foreach (int n in tablica2)
+            {
+                Console.Write($"{n} ");
+            }
+        }
     }
 }
